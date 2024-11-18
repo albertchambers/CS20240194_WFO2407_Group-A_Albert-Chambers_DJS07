866b2adf5f351e6b9d91edb97b99bc8bacbda980
Albert Chambers <albertchambers09@gmail.com>
2024-11-18 09:39:33 +0200

    fix startup code

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src="./images/rubber-duck.jpeg"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
