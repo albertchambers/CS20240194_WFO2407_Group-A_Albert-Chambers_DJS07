4b14d355ddc81c617f9f0e232ad7681f81bed9ad
Albert Chambers <albertchambers09@gmail.com>
2024-11-18 15:36:56 +0200

    create random first load image fetch and add JDOC

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -2,23 +2,48 @@
 
 import React from "react";
 
+/**
+ * Meme Generator
+ * @typedef {Object} Meme
+ * @property {string} id - The unique identifier for the meme.
+ * @property {string} name - The name of the meme.
+ * @property {string} url - The URL of the meme image.
+ * @property {number} width - The width of the meme image.
+ * @property {number} height - The height of the meme image.
+ * @property {number} box_count - The number of text boxes on the meme.
+ */
+
+/** @type {Meme[]} */
+const initialMemes = [];
+
 export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: ""
     });
-    const [allMemes, setAllMemes] = React.useState([])
-    
+
+    const [allMemes, setAllMemes] = React.useState(initialMemes);
+
+    // Fetch memes and set a random image on load
     React.useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
-    }, []) // Dependency Array
+            .then(data => {
+                const memes = data.data.memes;
+                setAllMemes(memes);
+                const randomNumber = Math.floor(Math.random() * memes.length);
+                setMeme(prevMeme => ({
+                    ...prevMeme,
+                    randomImage: memes[randomNumber].url
+                }));
+            });
+    }, []); // Dependency Array
 
     function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
+        if (allMemes.length === 0) return; // Safeguard in case the array is empty
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomNumber].url;
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
